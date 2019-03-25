94ab6a8801bcc62f32eefb5f0b64fe66a109d6e7
nahyun902 <nahyun902@naver.com>
2019-03-26 00:26:19 +0900

    새로운 과제(Example5)를 생성한다.

---
+++ Example5/Example5.cpp
@@ -0,0 +1,49 @@
+﻿
+#include "pch.h"
+#include <iostream>
+
+#include <CoalaMOD.h>
+#pragma comment(lib, "CoalaMOD.lib")
+#include <math.h>
+
+
+int main()
+{
+	int px, py, pz;
+	getPlayerLocation(&px, &py, &pz);
+	double dir = getPlayerDirection();
+
+	BlockID a = createBlock(BLOCK_GOLD);
+
+	//1층
+
+	locateBlock(a, px, py, pz);
+	locateBlock(a, px + 1, py, pz - 1);
+	locateBlock(a, px + 2, py, pz - 2);
+	locateBlock(a, px + 1, py, pz + 1);
+	locateBlock(a, px + 2, py, pz - 2);
+	locateBlock(a, px + 3, py, pz - 1);
+	locateBlock(a, px + 4, py, pz);
+	locateBlock(a, px + 3, py, pz + 1);
+
+	//2층
+	locateBlock(a, px, py, pz);
+	locateBlock(a, px + 1, py + 1, pz - 1);
+	locateBlock(a, px + 2, py + 1, pz - 2);
+	locateBlock(a, px + 1, py + 1, pz + 1);
+	locateBlock(a, px + 2, py + 1, pz - 2);
+	locateBlock(a, px + 3, py + 1, pz - 1);
+	locateBlock(a, px + 4, py + 1, pz);
+	locateBlock(a, px + 3, py + 1, pz + 1);
+
+
+	//3층
+	locateBlock(a, px + 1, py + 2, pz);
+	locateBlock(a, px + 2, py + 2, pz - 1);
+	locateBlock(a, px + 3, py + 2, pz);
+	locateBlock(a, px + 1, py + 2, pz + 1);
+
+	//
+	locateBlock(a, px + 2, py + 3, pz);
+}
+
